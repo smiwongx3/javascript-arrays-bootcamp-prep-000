bc3f76e06a8386b6fffd1fb851739126fb5c9c45
Samantha Wong <smiwong920@gmail.com>
2019-01-25 23:57:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,21 +1,2 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element)  {
-  return[element, ...array]
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element)  {
-  return[array(element)]
-}
-
-function addElementToEndOfArray(array, element)  {
-  return[...array, element]
-}
-
-function destructivelyAddElementToEndOfArray(array, element)  {
-  return[array(element)]
-}
-
-function accessElementInArray(array, element)  {
-  return
-}
