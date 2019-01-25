ede17645c983e88e73d2420d075592736bb8b8b8
Samantha Wong <smiwong920@gmail.com>
2019-01-25 23:47:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToEndOfArray(arrays, element)  {
-  return[arrays, ...element]
+  return[...arrays, element]
 }
