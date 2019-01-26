d7cf61999a4c1950920541a0645619eacd85086b
Samantha Wong <smiwong920@gmail.com>
2019-01-26 00:14:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,14 +5,9 @@ function addElementToBeginningOfArray(array, element)  {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element)  { 
-  return(array.unshift(element))
+  return[array.unshift(element)]
   }
   
   function addElementToEndOfArray(array, element)  {
   return[...array, element]
 }
-
-function destructivelyAddElementToEndOfArray(array, element)  { 
-  return(array.push(element))
-  }
-  
